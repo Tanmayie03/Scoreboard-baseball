b8bef20addde2823916b199f054d50c48ad01173
Tanmayie03 <tanmayiepatil@gmail.com>
2023-01-03 20:46:54 +0530

    Commit initial main

---
+++ index.js
@@ -0,0 +1,39 @@
+//for home
+let result= document.getElementById("home")
+console.log(result)
+let count=0
+function b1(){
+    console.log("+1 added")
+    count +=1  
+    result.textContent= count
+    }
+function b2(){
+    console.log("+1 added")
+    count +=2  
+    result.textContent= count
+    }
+function b3(){
+    console.log("+1 added")
+    count +=3 
+    result.textContent= count
+    }
+ 
+//for guest 
+let resultant = document.getElementById("guest")
+console.log(resultant)
+let num=0
+function g1(){
+    console.log("+1 added")
+    num +=1  
+    resultant.textContent= num
+    }
+function g2(){
+    console.log("+2 added")
+    num +=2  
+    resultant.textContent= num
+    }
+function g3(){
+    console.log("+3 added")
+    num +=3 
+    resultant.textContent= num
+    }
